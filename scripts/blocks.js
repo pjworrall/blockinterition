33b70507f325e0cbe871e440f1db9f561f041b02
Paul Worrall <paul.worrall@interition.net>
2018-07-06 09:28:12 +0100

    added input/fragments into repo and created transactions fragement to start mapping tx

--- scripts/blocks.js
+++ scripts/blocks.js
@@ -5,7 +5,7 @@ const parse = require('csv-parse');
 const readline = require('readline');
 const stream = require('stream');
 
-const chaindata = '../data/input/summary/blocks.csv';
+const chaindata = '../data/input/fragments/blocks.csv';
 
 const templatefile = '../map/block.map';
 
